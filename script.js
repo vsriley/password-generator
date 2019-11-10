4bb71ed0d24f3208c88a4f1fab71e4bb4afa2343
Valerie Riley <valeriesriley@gmail.com>
2019-11-09 22:25:07 -0700

    added way to ensure if user inputs typed word of number it is converted to integer version

--- script.js
+++ script.js
@@ -31,7 +31,12 @@
         //prompts user for length of password
         numCharacters = prompt("Enter the length you would like your password to be (between 8 and 128 characters): ");
         console.log("numCharacters: ", numCharacters);
-        //create way to ensure number and word version of number convert to integer?
+        //converts word version of number to integer
+        var numWords = ["eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine", "eighty", "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine", "ninety", "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine", "one hundred", "one hundred one", "one hundred two", "one hundred three", "one hundred four", "one hundred five", "one hundred six", "one hundred seven", "one hundred eight", "one hundred nine", "one hundred ten", "one hundred eleven", "one hundred twelve", "one hundred thirteen", "one hundred fourteen", "one hundred fifteen", "one hundred sixteen", "one hundred seventeen", "one hundred eighteen", "one hundred nineteen", "one hundred twenty", "one hundred twenty-one", "one hundred twenty-two", "one hundred twenty-three", "one hundred twenty-four", "one hundred twenty-five", "one hundred twenty-six", "one hundred twenty-seven", "one hundred twenty-eight"];
+        if(numWords.includes(numCharacters)){
+            numCharacters = numWords.indexOf(numCharacters) + 8;
+            console.log("Changing word of number to number characters: ", numCharacters);
+        }
         //ensures the user chooses a valid password length
         while(numCharacters < 8 || numCharacters > 128){
             numCharacters = prompt("Please enter a password length between 8 and 128 characters");
